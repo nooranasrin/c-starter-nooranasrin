ad51e0da2e60f159e86e10eae71bbff730e7b803
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-28 16:38:26 +0530

    created function isOdd that will give 0 when the number is even, 1 when the number is odd

--- basic_functions.c
+++ basic_functions.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
 int isEven(int);
+int isOdd(int);
 
 int isEven(int number) {
   return number % 2 == 0;
+}
+
+int isOdd(int number) {
+  return !isEven(number);
 }
 
 int main(void) {
@@ -11,5 +16,6 @@ int main(void) {
   printf("Enter a number\n");
   scanf("%d", &number);
   printf("The given number is even : %d\n", isEven(number));
+  printf("The given number is odd : %d\n", isOdd(number));
   return 0;
 }
