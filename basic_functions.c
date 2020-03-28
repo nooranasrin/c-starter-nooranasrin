837d76b75fee129be55e0e99e560bde82289aa3b
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-28 17:24:54 +0530

    created a function to calculate the simple interest

--- basic_functions.c
+++ basic_functions.c
@@ -4,6 +4,7 @@ int isEven(int);
 int isOdd(int);
 int getSquare(int);
 int getCube(int);
+int findSimpleInterest(int, int, int);
 
 int isEven(int number) {
   return number % 2 == 0;
@@ -21,13 +22,23 @@ int getCube(int number) {
   return getSquare(number) * number;
 }
 
+int findSimpleInterest(int p, int n, int r) {
+  return (p * n * r) / 100;
+}
+
 int main(void) {
-  int number;
+  int num1, p, n, r;
   printf("Enter a number\n");
-  scanf("%d", &number);
-  printf("The given number is even : %d\n", isEven(number));
-  printf("The given number is odd : %d\n", isOdd(number));
-  printf("The square of %d is %d\n", number, getSquare(number));
-  printf("The cube of %d is %d\n", number, getCube(number));
+  scanf("%d", &num1);
+
+  printf("%d is %s\n", num1, isEven(num1) ? "even" : "odd");
+  printf("%d is %s\n", num1, isOdd(num1) ? "odd" : "even");
+  printf("The square of %d is %d\n", num1, getSquare(num1));
+  printf("The cube of %d is %d\n", num1, getCube(num1));
+
+  printf("Enter principle amount, years, rate of interest to find SI\n");
+  scanf("%d%d%d", &p, &n, &r);
+  printf("The simple interest is %d\n", findSimpleInterest(p, n, r));
+
   return 0;
 }
