3547ee25b693a32c9d9b7b54ec71df12c2c9465d
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-28 17:54:35 +0530

    created a function that will give the average of three numbers

--- basic_functions.c
+++ basic_functions.c
@@ -5,6 +5,7 @@ int is_odd(int);
 int get_square(int);
 int get_cube(int);
 int find_simpleInterest(int, int, int);
+int find_average(int, int, int);
 
 int is_even(int number) {
   return number % 2 == 0;
@@ -26,16 +27,20 @@ int find_simpleInterest(int p, int n, int r) {
   return (p * n * r) / 100;
 }
 
+int find_average(int num1, int num2, int num3) {
+  return (num1 + num2 + num3) / 3;
+}
 
 int main(void) {
-  int num1, p, n, r;
-  printf("Enter a number\n");
-  scanf("%d", &num1);
+  int num1, num2, num3, p, n, r;
+  printf("Enter 3 number\n");
+  scanf("%d%d%d", &num1, &num2, &num3);
 
   printf("%d is %s\n", num1, is_even(num1) ? "even" : "odd");
   printf("%d is %s\n", num1, is_odd(num1) ? "odd" : "even");
   printf("The square of %d is %d\n", num1, get_square(num1));
   printf("The cube of %d is %d\n", num1, get_cube(num1));
+  printf("The average of %d, %d and %d : %d\n", num1, num2, num3, find_average(num1, num2, num3));
 
   printf("Enter principle amount, years, rate of interest to find Interest\n");
   scanf("%d%d%d", &p, &n, &r);
