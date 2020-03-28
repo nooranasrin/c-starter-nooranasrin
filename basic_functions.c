4d97109cf1aff86398d8d71b268a7daee9e64419
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-28 16:42:19 +0530

    created function getSquare that will give the square of the given number

--- basic_functions.c
+++ basic_functions.c
@@ -2,6 +2,7 @@
 
 int isEven(int);
 int isOdd(int);
+int getSquare(int);
 
 int isEven(int number) {
   return number % 2 == 0;
@@ -11,11 +12,16 @@ int isOdd(int number) {
   return !isEven(number);
 }
 
+int getSquare(int number) {
+  return number * number;
+}
+
 int main(void) {
   int number;
   printf("Enter a number\n");
   scanf("%d", &number);
   printf("The given number is even : %d\n", isEven(number));
   printf("The given number is odd : %d\n", isOdd(number));
+  printf("The square of %d is %d\n", number, getSquare(number));
   return 0;
 }
