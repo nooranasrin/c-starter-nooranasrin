72052db6c30cadae548e76359f24eeaf92d0c208
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-28 18:11:19 +0530

    added function to find the largest of three numbers

--- basic_functions.c
+++ basic_functions.c
@@ -5,6 +5,8 @@ int is_odd(int);
 int get_square(int);
 int get_cube(int);
 int find_simpleInterest(int, int, int);
+int find_greatest(int, int);
+int find_greatest_of_three(int, int, int);
 int find_average(int, int, int);
 
 int is_even(int number) {
@@ -27,6 +29,14 @@ int find_simpleInterest(int p, int n, int r) {
   return (p * n * r) / 100;
 }
 
+int find_greatest(int firstNum, int secondNum) {
+  return firstNum > secondNum ? firstNum : secondNum;
+}
+
+int find_greatest_of_three(int num1, int num2, int num3) {
+  return find_greatest(find_greatest(num1, num2), num3);
+}
+
 int find_average(int num1, int num2, int num3) {
   return (num1 + num2 + num3) / 3;
 }
@@ -40,6 +50,7 @@ int main(void) {
   printf("%d is %s\n", num1, is_odd(num1) ? "odd" : "even");
   printf("The square of %d is %d\n", num1, get_square(num1));
   printf("The cube of %d is %d\n", num1, get_cube(num1));
+  printf("The largest of %d, %d and %d is %d\n", num1, num2, num3, find_greatest_of_three(num1, num2, num3));
   printf("The average of %d, %d and %d : %d\n", num1, num2, num3, find_average(num1, num2, num3));
 
   printf("Enter principle amount, years, rate of interest to find Interest\n");
