129ae965d1359d4d44e43ac3c9f397be648644e1
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-29 12:12:14 +0530

    added function to print the fibonaocci series upto a limit and modified factorial funtion return type

--- loops.c
+++ loops.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 
-int find_factorial(int);
+unsigned long int find_factorial(int);
+void print_fibonacci(int);
 
-int find_factorial(int term) {
-  int factorial = 1, number;
+unsigned long int find_factorial(int term) {
+  long factorial = 1, number;
   for (int number = 1; number <= term; number++) {
     factorial = factorial * number;
   }
   return factorial;
+}
+
+void print_fibonacci(int limit) {
+  int previous = 0, current = 0, next = 1;
+  while (current <= limit) {
+    printf("%d\n",current);
+    previous = current;
+    current = next;
+    next = previous + current;
+  }
 }
 
 int main(void) {
@@ -15,6 +26,8 @@ int main(void) {
   printf("Enter a number\n");
   scanf("%d", &number);
 
-  printf("The factorial of %d : %d\n", number, find_factorial(number));
+  printf("\nThe factorial of %d : %lu\n", number, find_factorial(number));
+  printf("\nThe fibonacci series till %d are :\n", number);
+  print_fibonacci(number);
   return 0;
 }
