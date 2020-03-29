846c783b0de09305d32cb4e10ef753509b6a8f7d
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-29 12:41:33 +0530

    added function to print odd numbers from 1 to N

--- loops.c
+++ loops.c
@@ -2,6 +2,17 @@
 
 unsigned long int find_factorial(int);
 void print_fibonacci(int);
+void print_odd(int, int);
+
+void print_odd(int start, int end) {
+  int skip = 2, current;
+  int excess = start % skip;
+  int firstOdd = start - excess + 1;
+
+  for(current = firstOdd; current <= end; current += skip){
+    printf("%d\n", current);
+  }
+}
 
 unsigned long int find_factorial(int term) {
   long factorial = 1, number;
@@ -14,7 +25,7 @@ unsigned long int find_factorial(int term) {
 void print_fibonacci(int limit) {
   int previous = 0, current = 0, next = 1;
   while (current <= limit) {
-    printf("%d\n",current);
+    printf("%d\n",current);    
     previous = current;
     current = next;
     next = previous + current;
@@ -29,5 +40,7 @@ int main(void) {
   printf("\nThe factorial of %d : %lu\n", number, find_factorial(number));
   printf("\nThe fibonacci series till %d are :\n", number);
   print_fibonacci(number);
+  printf("\nThe odd numbers from 1 to %d are :\n", number);
+  print_odd(1, number);
   return 0;
 }
